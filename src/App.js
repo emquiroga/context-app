9d0d1bd9d80e3f56871077cc0e241af1061229c7
Emiliano <emiliano.quiroga093@gmail.com>
2021-10-21 20:48:01 -0300

    app finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { UserContext } from './contexts/UserContext'
+import AppRouter from './routes/AppRouter'
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value={{user, setUser}}>
+      <AppRouter />
+    </UserContext.Provider>
+  )
 }
 
-export default App;
+export default App
